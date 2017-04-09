a6c38ec8dd57ce1feb22aad2a129a3d0624e1d4d
SJS <ohhapday@gmail.com>
2017-04-09 18:24:32 +0900

    4/9 첫 커밋

---
+++ application/controllers/Test.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SJS
+ * Date: 2017-04-09
+ * Time: 오후 3:31
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test extends CI_Controller {
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $this->load->view('welcome_message');
+
+        $query = "
+            SELECT * FROM db_test
+        ";
+        $result = $this->db->query($query)->result();
+
+        var_dump($result);
+    }
+}
